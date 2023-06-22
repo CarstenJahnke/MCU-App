1ca64b4a445c9ec6395fcb8f1fc2ed11b03b027b
Carsten Jahnke <carsten@jahnke.sh>
2023-06-22 08:11:40 +0200

    fixing test fail

--- components/Heading/index.test.js
+++ components/Heading/index.test.js
@@ -1,11 +1,6 @@
-// import Heading from ".";
-// import { render, screen } from "@testing-library/react";
+import Heading from ".";
+import { render, screen } from "@testing-library/react";
 
-// test("test", () => {
-//   render(<Heading>🐬</Heading>);
-//   const element = screen.getByText("🐬");
-//   expect(element).toBeInTheDocument();
-// });
 test("test", () => {
   render(<Heading>Marvelous Cinematic Unisearch</Heading>);
   const element = screen.getByText("Marvelous Cinematic Unisearch");
