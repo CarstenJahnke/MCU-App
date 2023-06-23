94380d9d98e85422ff7c6fe3abaf30da992f200b
Carsten Jahnke <carsten@jahnke.sh>
2023-06-23 13:57:48 +0200

    adding the styling for the phases

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -5,6 +5,8 @@ import {
   StyledMovieImage,
   StyledMovieTitle,
   MovieCardsList,
+  StyledPhaseCard,
+  StyledPhaseHeadline,
 } from "../styling/MovieCardsStyling";
 import Image from "next/image";
 import { apikey } from "../../pages/_app";
@@ -30,49 +32,67 @@ const MovieCards = () => {
     return <LoadingStyling>Filme werden geladen...</LoadingStyling>;
   }
 
+  // MCU-Phasen Veröffentlichungsjahre
+  const mcuPhases = [
+    { phase: 1, startYear: 2008, endYear: 2012 }, // Phase 1 hinzugefügt
+    { phase: 2, startYear: 2013, endYear: 2015 }, // Phase 2 hinzugefügt
+    { phase: 3, startYear: 2016, endYear: 2019 }, // Phase 3 hinzugefügt
+    { phase: 4, startYear: 2020, endYear: 2022 }, // Phase 4 hinzugefügt
+    { phase: 5, startYear: 2023, endYear: 2025 }, // Phase 5 hinzugefügt
+    { phase: 6, startYear: 2026, endYear: 2028 }, // Phase 6 hinzugefügt
+  ];
+
   // Filter and sort movies from 2008
   const filteredMovies = movies.filter(
     (movie) => new Date(movie.release_date).getFullYear() >= 2008
   );
 
-  filteredMovies.sort((a, b) => {
-    const yearA = new Date(a.release_date).getFullYear();
-    const yearB = new Date(b.release_date).getFullYear();
-    if (yearA === 2008 && yearB === 2008) {
-      // Sort by release date if both movies are from 2008
-      const releaseDateA = new Date(a.release_date);
-      const releaseDateB = new Date(b.release_date);
-      return releaseDateA - releaseDateB;
-    } else if (yearA === 2008) {
-      // Prioritize the film from 2008 in const yearA
-      return -1;
-    } else if (yearB === 2008) {
-      // Prioritize the film from 2008 in const yearB
-      return 1;
-    } else {
-      // Sort by year of publication
-      return yearA - yearB;
-    }
+  // Sort movies by phases
+  const sortedMovies = [];
+  mcuPhases.forEach((phase) => {
+    const moviesInPhase = filteredMovies.filter((movie) => {
+      const releaseYear = new Date(movie.release_date).getFullYear();
+      return releaseYear >= phase.startYear && releaseYear <= phase.endYear;
+    });
+    sortedMovies.push(...moviesInPhase);
   });
 
   return (
     <MovieCardsList>
-      {filteredMovies.map((movie) => (
-        <Link href={`/movies/${encodeURIComponent(movie.id)}`} key={movie.id}>
-          <StyledMovieCard>
-            <StyledMovieImage>
-              <Image
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                alt={movie.name}
-                width={200}
-                height={300}
-              />
-            </StyledMovieImage>
-            <StyledMovieTitle>
-              {movie.title} ({new Date(movie.release_date).getFullYear()})
-            </StyledMovieTitle>
-          </StyledMovieCard>
-        </Link>
+      {mcuPhases.map((phase) => (
+        <StyledPhaseCard key={`PhaseCard${phase.phase}`}>
+          <div className="movies-container">
+            <StyledPhaseHeadline>Phase {phase.phase}</StyledPhaseHeadline>
+            {sortedMovies
+              .filter((movie) => {
+                const releaseYear = new Date(movie.release_date).getFullYear();
+                return (
+                  releaseYear >= phase.startYear && releaseYear <= phase.endYear
+                );
+              })
+              .map((movie) => (
+                <Link
+                  href={`/movies/${encodeURIComponent(movie.id)}`}
+                  key={movie.id}
+                >
+                  <StyledMovieCard>
+                    <StyledMovieImage>
+                      <Image
+                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                        alt={movie.name}
+                        width={200}
+                        height={300}
+                      />
+                    </StyledMovieImage>
+                    <StyledMovieTitle>
+                      {movie.title} (
+                      {new Date(movie.release_date).getFullYear()})
+                    </StyledMovieTitle>
+                  </StyledMovieCard>
+                </Link>
+              ))}
+          </div>
+        </StyledPhaseCard>
       ))}
     </MovieCardsList>
   );
