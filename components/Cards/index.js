369aca37e88d53783aed60f4e5e4fdb8d7bfe6a5
Carsten Jahnke <carsten@jahnke.sh>
2023-06-28 17:25:16 +0200

    refactor LoadingScreen

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -12,15 +12,14 @@ import {
 } from "../styling/MovieCardsStyling";
 import Image from "next/image";
 import { apikey } from "../../pages/_app";
-import { LoadingImage, LoadingStyle } from "../styling/LoadingStyling";
 import GlobalStyle from "../../styles";
 import { mcuTimeline } from "../MCUTimeline";
 import ButtonStyle from "../styling/ButtonStyling";
 import { motion } from "framer-motion";
 import FavoriteButton from "../FavoriteButton/FavButton";
 import { mcuPhases } from "../Phases";
+import LoadingScreen from "../LoadingScreen";
 
-// Funktion zum Abrufen der Daten von der URL, die Elemente aus der Antwort zurück gibt
 const fetcher = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
@@ -28,73 +27,35 @@ const fetcher = async (url) => {
 };
 
 const MovieCards = () => {
-  // Zustandsvariablen für den Ladezustand, den ersten Ladezustand und die Sortieroption
-  const [isLoading, setIsLoading] = useState(true);
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
-  const [sortOption, setSortOption] = useState(1); // 1: Nach Phasen sortiert, 2: Chronologisch sortiert
-
-  // Abrufen der Film-Daten mithilfe des useSWR-Hooks
   const { data: movies, error } = useSWR(
     `https://api.themoviedb.org/3/list/8258181?api_key=${apikey}&language=de`,
     fetcher
   );
 
-  const [isFadeOut, setIsFadeOut] = useState(false);
+  const [sortOption, setSortOption] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
-  // Wird einmalig ausgeführt, wenn die Komponente geladen wird, um einen Ladescreen zu erzeugen
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
       setIsFirstLoad(true);
-      setIsFadeOut(true); // Füge diese Zeile hinzu, um den Ladescreen auszublenden
     }, 1000);
 
     return () => clearTimeout(timer);
   }, []);
 
-  // Setzt isFirstLoad zurück, wenn sich die Film-Daten ändern
   useEffect(() => {
     setIsFirstLoad(true);
   }, [movies]);
 
-  // Wenn ein Fehler auftritt, wird eine Fehlermeldung angezeigt
   if (error) {
     return <p>Daten konnten nicht abgerufen werden.</p>;
   }
 
-  // Zeigt den Ladezustand an
   if (isLoading && isFirstLoad) {
-    return (
-      <>
-        <LoadingStyle
-          as={motion.div}
-          initial={{ opacity: 0 }} // Anfangszustand der Animation
-          animate={{ opacity: 1 }} // Zustand, zu dem animiert werden soll
-          transition={{ duration: 0.5 }} // Dauer der Animation
-        >
-          <LoadingImage />
-        </LoadingStyle>
-        <LoadingStyle
-          as={motion.div}
-          initial={{ opacity: 0 }} // Anfangszustand der Animation
-          animate={{ opacity: 1 }} // Zustand, zu dem animiert werden soll
-          transition={{ duration: 0.25 }} // Dauer der Animation
-        >
-          Arc-Reaktor wird geladen...
-        </LoadingStyle>
-      </>
-    );
+    return <LoadingScreen />;
   }
-
-  // // MCU-Phasen Veröffentlichungsjahre
-  // const mcuPhases = [
-  //   { phase: 1, startYear: 2008, endYear: 2012 },
-  //   { phase: 2, startYear: 2013, endYear: 2015 },
-  //   { phase: 3, startYear: 2016, endYear: 2019 },
-  //   { phase: 4, startYear: 2020, endYear: 2022 },
-  //   { phase: 5, startYear: 2023, endYear: 2025 },
-  //   { phase: 6, startYear: 2026, endYear: 2028 },
-  // ];
 
   // Filtern und Sortieren der Filme ab 2008
   const filteredMovies = movies
