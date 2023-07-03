ed76c2224b2f8063bf6047a8cdb03a9ba9ba1cd8
Carsten Jahnke <carsten@jahnke.sh>
2023-07-03 20:13:50 +0200

    add some questions for the quiz

--- components/Quiz/Questions.js
+++ components/Quiz/Questions.js
@@ -1,24 +1,208 @@
 export const initialQuestions = [
   {
-    question: "Frage 1",
-    answers: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4"],
+    question: "Wie viele Infinity Steine gibt es?",
+    answers: ["3", "4", "5", "6"],
+    correctAnswerIndex: 3,
+  },
+
+  {
+    question: "Welche Automarke wird prominent in Iron Man gefahren? ",
+    answers: ["Porsche", "Audi", "Mercedes", "Ferrari"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question: "Wie heißt der mutierte Widersacher von der unglaubliche Hulk? ",
+    answers: ["Red Hulk", "Abomination", "The Thing", "Kraven"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question:
+      "Welches Alias verwendet Natasha Romanoff als Tony Starks Assistentin in Iron Man 2?",
+    answers: ["Black Mamba", "Evelyn Potts", "Gwen Stacy", "Nathalie Rushman"],
+    correctAnswerIndex: 3,
+  },
+
+  {
+    question:
+      "In welchem US-Bundesstaat stranden Thor und sein Hammer Mjölnir?",
+    answers: ["Hawaii", "Nebraska", "New Mexico", "Washington"],
+    correctAnswerIndex: 2,
+  },
+
+  {
+    question:
+      "Wie lautet Buckys vollständiger Name, den wir in Captain America The First Avenger hören?",
+    answers: [
+      "Ed Brubaker Barnes",
+      "James Buchanan Barnes",
+      "Stanley Barnes",
+      "Scott Wright Barnes",
+    ],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question:
+      "An welchem Ort fand die Mission von Clint und Natasha statt, an die sie sich stets anders erinnern?",
+    answers: ["Budapest", "Brüssel", "Prag", "Helsinki"],
     correctAnswerIndex: 0,
   },
+
+  {
+    question:
+      "Der Äther, den wir in Thor 2 The Dark Kingdom erstmals gesehen haben, ist die flüssige Form welches Infinity-Steins?",
+    answers: [
+      "Der Realitätsstein",
+      "Der Seelenstein",
+      "Der Machtstein",
+      "Der Gedankenstein",
+    ],
+    correctAnswerIndex: 0,
+  },
+
+  {
+    question: "Wie heißt Scott Langs Tochter in Ant-Man?",
+    answers: ["Barbara", "Cassie", "Morgan", "Mary-Jane"],
+    correctAnswerIndex: 2,
+  },
+
+  {
+    question:
+      "An welchem deutschen Flughafen bekämpfen sich die Avengers in The First Avenger Civil War?",
+    answers: [
+      "Köln/Bonn",
+      "Frankfurt am Main",
+      "Leipzig/Halle",
+      "Berlin Schönefeld",
+    ],
+    correctAnswerIndex: 2,
+  },
+
+  {
+    question: "Wie heißt das Wifi-Passwort in Doctor Strange?",
+    answers: ["Agamotto", "Iyengar", "Abra Kadabra", "Shamballa"],
+    correctAnswerIndex: 3,
+  },
+
+  {
+    question:
+      "Wie heißt die K.I. im Anzug von Spider-Man in Spider-Man Homecoming?",
+    answers: ["Sarah", "Hallie", "Karen", "May"],
+    correctAnswerIndex: 2,
+  },
+
+  {
+    question:
+      "Wie viele mögliche Zukunftsvariationen sieht Doctor Strange in Avengers Infinity War?",
+    answers: ["9.999.999", "14.000.605", "28.000.740", "32.000.616"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question: "Welcher Alienrasse gehört Goose in Captain Marvel an?",
+    answers: ["Skrull", "Flerken", "Aspirant", "Tribbitite"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question:
+      "In welcher Stadt lebt Sam Wilsons Schwester mitsamt Familie in The Falcon and the Winter Soldier?",
+    answers: [
+      "Delacroix, Louisiana",
+      "Atlanta, Georgia",
+      "Prattville, Alabama",
+      "Tampa, Florida",
+    ],
+    correctAnswerIndex: 0,
+  },
+
+  {
+    question:
+      "In welchem Jahr wurde der erste Iron-Man Film veröffentlicht, der das Marvel Cinematic Universe startete?",
+    answers: ["2005", "2008", "2010", "2012"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question: "Wie heißt Thors Hammer?",
+    answers: ["Vanir", "Mjolnir", "Asen", "Norn"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question:
+      "Was erzählt Tony im Incredible Hulk Thaddeus Ross am Ende des Films?",
+    answers: [
+      "Dass er The Hulk studieren will",
+      "Dass er von SHIELD weiß",
+      "Dass sie ein Team zusammenstellen",
+      "Dieser Thaddäus schuldet ihm Geld",
+    ],
+    correctAnswerIndex: 2,
+  },
+
+  {
+    question: "Woraus besteht der Schild von Captain America",
+    answers: ["Adamantium", "Vibranium", "Promethium", "Karbonadium"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question:
+      "Die Flerkens sind eine Rasse extrem gefährlicher Aliens die was ähnelt?",
+    answers: ["Katzen", "Enten", "Reptilien", "Waschbären"],
+    correctAnswerIndex: 0,
+  },
+
+  {
+    question: "Wie heißt der KI-Buttler von Iron-Man, bevor er Vision wird?",
+    answers: ["Homer", "Jarvis", "Alfred", "Marvin"],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question: "Wie heißt The Black Panther wirklich?",
+    answers: ["T Challa", "M Baku", "N Jadaka", "N Jobu"],
+    correctAnswerIndex: 0,
+  },
+
+  {
+    question:
+      "Was ist die Alien-Rasse, die Loki sendet um in The Avengers in die Erde einzudringen?",
+    answers: ["Die Chitauri", "Die Skrulls", "Der Kree", "Die Flerkens"],
+    correctAnswerIndex: 0,
+  },
+
+  {
+    question:
+      "Wer war der letzte Inhaber der Raumstein bevor Thanos es für seinen Infinity Handschuh beansprucht?",
+    answers: ["Thor", "Loki", "Der Collector", "Tony Stark"],
+    correctAnswerIndex: 2,
+  },
+
+  {
+    question:
+      "Was waren die drei Gegenstände die Rocket braucht um aus dem Gefängnis zu fliehen?",
+    answers: [
+      "Sicherheitskarte, Gabel und ein Knöchelmonitor",
+      "Sicherheitsband, Batterie und eine Beinprothese",
+      "Fernglas, Zünder und eine Beinprothese",
+      "Messer, Kabeldrähte und Peters Mixtape",
+    ],
+    correctAnswerIndex: 1,
+  },
+
+  {
+    question: "Wie heißt die Schwester von Black Panther?",
+    answers: ["Shuri", "Nakia", "Ramonda", "Okoye"],
+    correctAnswerIndex: 0,
+  },
+
   {
     question: "Frage 2",
     answers: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4"],
     correctAnswerIndex: 2,
   },
-  {
-    question: "Frage 3",
-    answers: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4"],
-    correctAnswerIndex: 2,
-  },
-  {
-    question: "Frage 4",
-    answers: ["Antwort 1", "Antwort 2", "Antwort 3", "Antwort 4"],
-    correctAnswerIndex: 1,
-  },
-
-  // Weitere Fragen hier hinzufügen
 ];
