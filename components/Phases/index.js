27198408a4a9be61573aab544837370026217601
Carsten <130060363+CarstenJahnke@users.noreply.github.com>
2024-05-21 10:40:34 +0200

    Update index.js

--- components/Phases/index.js
+++ components/Phases/index.js
@@ -2,8 +2,8 @@
 export const mcuPhases = [
   { phase: 1, startYear: 2008, endYear: 2012 },
   { phase: 2, startYear: 2013, endYear: 2015 },
-  { phase: 3, startYear: 2016, endYear: 2017 },
-  { phase: 4, startYear: 2018, endYear: 2022 },
+  { phase: 3, startYear: 2016, endYear: 2019 },
+  { phase: 4, startYear: 2020, endYear: 2022 },
   { phase: 5, startYear: 2023, endYear: 2025 },
   { phase: 6, startYear: 2026, endYear: 2028 },
 ];
