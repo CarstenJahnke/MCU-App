bfc679f2c48993a90cc487fe2eef63ece2709032
Carsten <130060363+CarstenJahnke@users.noreply.github.com>
2024-05-21 11:32:26 +0200

    Update index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -11,27 +11,24 @@ import React, { useState, useEffect } from "react";
 import useSWR from "swr";
 import { ButtonGeneralContainer } from "../Buttons/ButtonGeneralContainer";
 import Quiz from "../Quiz";
-
 // Funktion zum Abrufen der Daten von der API
 const fetcher = async (url) => {
   const response = await fetch(url);
   const data = await response.json();
   return data.items;
 };
-
 // Funktion zum Abrufen des Jahres eines Films basierend auf der Option (1 für Release-Jahr, 2 für chronologisches Jahr)
 export const getMovieYearFromTimeline = (movie, option) => {
   if (option === 1) {
     return `(${new Date(movie.release_date).getFullYear()})`;
   } else {
     const movieTitle = movie.title ? movie.title.toLowerCase() : "";
-    const movieFromTimeline = mcuTimeline.find((m) =>
-      movieTitle.includes(m.title.toLowerCase())
+    const movieFromTimeline = mcuTimeline.find((movie) =>
+      movieTitle.includes(movie.title.toLowerCase())
     );
     return movieFromTimeline ? `(${movieFromTimeline.year})` : "";
   }
 };
-
 // MovieCards-Komponente
 const MovieCards = () => {
   // Verwendung von useSWR-Hook zum Abrufen der Daten von der API
@@ -39,7 +36,6 @@ const MovieCards = () => {
     `https://api.themoviedb.org/3/list/12179?api_key=${apikey}&language=de`,
     fetcher
   );
-
   // Zustandsvariablen
   const [sortOption, setSortOption] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
@@ -49,29 +45,26 @@ const MovieCards = () => {
   const [filteredMoviesByFavourites, setFilteredMoviesByFavourites] = useState(
     []
   );
-  const [sortedMovies, setSortedMovies] = useState([]);
-
+  const [sortedMovies, setSortedMovies] = useState(
+    // Sortieren der Filme basierend auf der gewählten Option
+    []
+  );
   // Effekt-Hook zum Setzen des Ladezustands
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1000);
-
     return () => clearTimeout(timer);
-  }, []);
-
+  });
   useEffect(() => {
     handleSetSortMovies();
   }, [movies, sortOption]);
-
   useEffect(() => {
     updateFavouriteMoviesDisplay();
   }, [sortedMovies, showFavorites]);
-
   if (error) {
     return <StyledText>Daten konnten nicht abgerufen werden.</StyledText>;
   }
-
   if (isLoading) {
     return <LoadingScreen />;
   }
@@ -124,7 +117,6 @@ const MovieCards = () => {
       )}
     </>
   );
-
   function updateFavouriteMoviesDisplay() {
     setFilteredMoviesByFavourites(
       (() => {
@@ -142,18 +134,15 @@ const MovieCards = () => {
       })()
     );
   }
-
   // Funktion zum Umschalten des Quiz anzeigen
   function handleQuizButtonClick() {
     setShowQuiz(true);
   }
-
   // Funktion zum Hinzufügen/Entfernen von Favoriten
   function toggleFavoriteMovie(movieId) {
     const favorites = localStorage.getItem("favorites")
       ? JSON.parse(localStorage.getItem("favorites"))
       : [];
-
     if (favorites.includes(movieId)) {
       const updatedFavorites = favorites.filter((id) => id !== movieId);
       localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
@@ -166,19 +155,15 @@ const MovieCards = () => {
       const movie = sortedMovies.find((movie) => movie.id === movieId);
       setFavoriteMovies((prevFavorites) => [...prevFavorites, movie]); // Füge den Film zur favorisierten Filme-Liste hinzu
     }
-
     const updatedFavorites = localStorage.getItem("favorites")
       ? JSON.parse(localStorage.getItem("favorites"))
       : [];
-
     setShowFavorites(updatedFavorites.length > 0);
   }
-
   // Funktion zum Umschalten der Favoritenanzeigen
   function toggleFavorites() {
     setShowFavorites(!showFavorites); // Toggle den Favoritenanzeigen-Status
   }
-
   function getSortedMoviesByPhase() {
     let filteredMovies;
     // Filtern der Filme basierend auf dem Veröffentlichungsjahr
@@ -189,7 +174,6 @@ const MovieCards = () => {
     } else {
       filteredMovies = [];
     }
-
     // Sortieren der Filme nach Phasen
     const sortedMoviesByPhase = [];
     mcuPhases.forEach((phase) => {
@@ -203,25 +187,21 @@ const MovieCards = () => {
           const releaseYearB = new Date(b.release_date).getFullYear();
           return releaseYearA - releaseYearB;
         });
-
       sortedMoviesByPhase.push(...moviesInPhase);
     });
     return sortedMoviesByPhase;
   }
-
   function getSortedMoviesChronologically() {
     // Sortieren der Filme chronologisch
     const sortedMoviesChronological = getSortedMoviesByPhase().sort((a, b) => {
       const movieATitle = a.title;
       const movieBTitle = b.title;
-
-      const movieA = mcuTimeline.find((m) =>
-        movieATitle.includes(m.title)
+      const movieA = mcuTimeline.find((movie) =>
+        movieATitle.includes(movie.title)
       );
-      const movieB = mcuTimeline.find((m) =>
-        movieBTitle.includes(m.title)
+      const movieB = mcuTimeline.find((movie) =>
+        movieBTitle.includes(movie.title)
       );
-
       if (movieA && movieB) {
         return movieA.year.localeCompare(movieB.year);
       } else if (movieA) {
@@ -232,10 +212,8 @@ const MovieCards = () => {
         return 0;
       }
     });
-
     return sortedMoviesChronological;
   }
-
   function handleSetSortMovies() {
     setSortedMovies(
       sortOption === 1
@@ -244,5 +222,4 @@ const MovieCards = () => {
     );
   }
 };
-
 export default MovieCards;
